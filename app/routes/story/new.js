7c067ba68aeec701f7e780044f93afb0153965b5
Quentin61 <45429939+Quentin61@users.noreply.github.com>
2020-03-19 18:10:17 +0100

    Update :
    - ajout des tag dans le formulaire de stories
    - affichage des tags des stories

--- app/routes/story/new.js
+++ app/routes/story/new.js
@@ -9,21 +9,29 @@ export default Route.extend({
     return RSVP.hash({
       developer : this.get('store').findAll('developer'),
       story : EmberObject.create(),
+      tag : EmberObject.create(),
+      tags : this.get('store').findAll('tag'),
+      color : ['black','blue','green','orange','pink','purple','red','teal','yellow','positive','negative'],
       project : this.get('store').findRecord('project',project.project_id)
     })
   },
   actions:
     {
-      new(story, project)
+      new(story, project, tags)
       {
         story.set('project',project);
+        story.set('tags', tags);
         this.get('store').createRecord('story', story).save().then(()=>
             this.transitionTo('project',project.id)
         );
       },
-      cancel()
+      cancel(id)
       {
-          this.transitionTo('story')
+
+      },
+      addTag(tags)
+      {
+          this.get('store').createRecord('tag', tags).save();
       }
     }
 });
